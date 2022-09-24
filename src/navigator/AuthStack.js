a0ab5dfb6c6c2a872f220514b5235ae2ae08a8d0
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-24 18:08:17 -0500

    cjuidado

--- src/navigator/AuthStack.js
+++ src/navigator/AuthStack.js
@@ -3,15 +3,22 @@ import { createStackNavigator } from '@react-navigation/stack';
 import AuthScreen from '../pages/AuthScreen';
 import OnboardingCarousel from '../pages/OnboardingCarousel';
 import { GoogleSignin } from '@react-native-community/google-signin';
+import { Platform } from 'react-native';
 
 const Auth = createStackNavigator();
 
 export default function AuthStack() {
 
   useEffect(() => {
+    if (Platform) {
+
+    }
     GoogleSignin.configure({
-      webClientId: '1076046321611-09449lp6fhfd3gpslodmju6uioc143e5.apps.googleusercontent.com',
+      webClientId: Platform === 'ios' ? '1076046321611-jfpk32941r7aom0fmdsc02otj4ld593s.apps.googleusercontent.com' : '1076046321611-09449lp6fhfd3gpslodmju6uioc143e5.apps.googleusercontent.com'
+      // webClientId: '1076046321611 - jfpk32941r7aom0fmdsc02otj4ld593s.apps.googleusercontent.com'
+      // webClientId: '1076046321611-09449lp6fhfd3gpslodmju6uioc143e5.apps.googleusercontent.com',
     });
+
   }, [])
 
 
