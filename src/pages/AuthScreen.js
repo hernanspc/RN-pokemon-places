61c9d3c912b060741045863279051fcca1799330
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-24 13:05:05 -0500

    sign

--- src/pages/AuthScreen.js
+++ src/pages/AuthScreen.js
@@ -11,14 +11,52 @@ import {
 import FormInput from '../components/FormInput';
 import FormButton from '../components/FormButton';
 import SocialButton from '../components/SocialButton';
+import { GoogleSignin } from '@react-native-community/google-signin';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 const AuthScreen = () => {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
 
-    const handleSignIn = () => {
-        // googleLogin();
-        console.log('handleSignIn ', handleSignIn)
+    const handleSignIn = async () => {
+        console.log('handleSignIn ',)
+
+        try {
+            // Get the users ID token
+            const { idToken } = await GoogleSignin.signIn();
+
+            // Create a Google credential with the token
+            const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+            // Sign-in the user with the credential
+            await auth().signInWithCredential(googleCredential)
+                // Use it only when user Sign's up, 
+                // so create different social signup function
+                // .then(() => {
+                //   //Once the user creation has happened successfully, we can add the currentUser into firestore
+                //   //with the appropriate details.
+                //   // console.log('current User', auth().currentUser);
+                //   firestore().collection('users').doc(auth().currentUser.uid)
+                //   .set({
+                //       fname: '',
+                //       lname: '',
+                //       email: auth().currentUser.email,
+                //       createdAt: firestore.Timestamp.fromDate(new Date()),
+                //       userImg: null,
+                //   })
+                //   //ensure we catch any errors at this stage to advise us if something does go wrong
+                //   .catch(error => {
+                //       console.log('Something went wrong with added user to firestore: ', error);
+                //   })
+                // })
+                //we need to catch the whole sign up process if it fails too.
+                .catch(error => {
+                    console.log('Something went wrong with sign up: ', error);
+                });
+        } catch (error) {
+            console.log({ error });
+        }
     }
 
     return (
