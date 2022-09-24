12f7cc7c5decd864bc1d1f02d76dd53ff42b1047
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-24 13:12:39 -0500

    habilitar google

--- src/navigator/AuthStack.js
+++ src/navigator/AuthStack.js
@@ -1,11 +1,20 @@
+import React, { useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import AuthScreen from '../pages/AuthScreen';
 import OnboardingCarousel from '../pages/OnboardingCarousel';
-import React from 'react';
+import { GoogleSignin } from '@react-native-community/google-signin';
 
 const Auth = createStackNavigator();
 
 export default function AuthStack() {
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: '1076046321611-09449lp6fhfd3gpslodmju6uioc143e5.apps.googleusercontent.com',
+    });
+  }, [])
+
+
   return (
     <Auth.Navigator
       screenOptions={{
