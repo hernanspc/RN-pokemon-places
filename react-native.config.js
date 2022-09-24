2bada2427d03bef41ffac40aa70b7d549d9a5a20
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-24 17:23:15 -0500

    thank u good

---
+++ react-native.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    project: {
+        ios: {},
+        android: {}
+    },
+    dependencies: {
+        '@react-native-community/google-signin': {
+            platforms: {
+                ios: null,
+            },
+        },
+    },
+    assets: ['./assets/fonts/'],
+}
