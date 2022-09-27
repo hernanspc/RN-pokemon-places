9c24aaefa87e48f58b0432cb0a88275da4ef2d5a
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-09-26 19:04:16 -0500

    view

--- src/pages/CreateSquadScreen.js
+++ src/pages/CreateSquadScreen.js
@@ -118,46 +118,41 @@ const CreateSquadScreen = () => {
                 />
             </HStack>
 
-            <View
-                style={{ alignItems: 'center' }}
-            >
+            <FlatList
+                data={simpleListPokemon}
+                keyExtractor={(pokemon) => pokemon.id}
+                showsVerticalScrollIndicator={false}
+                numColumns={2}
 
-                <FlatList
-                    data={simpleListPokemon}
-                    keyExtractor={(pokemon) => pokemon.id}
-                    showsVerticalScrollIndicator={false}
-                    numColumns={2}
+                // Header
+                ListHeaderComponent={(
+                    <Text style={{
+                        ...styles.title,
+                        ...styles.globalMargin,
+                        top: top + 20,
+                        marginBottom: top + 20,
+                        paddingBottom: 10
+                    }}>Pokedex </Text>
+                )}
 
-                    // Header
-                    ListHeaderComponent={(
-                        <Text style={{
-                            ...styles.title,
-                            ...styles.globalMargin,
-                            top: top + 20,
-                            marginBottom: top + 20,
-                            paddingBottom: 10
-                        }}>Pokedex </Text>
-                    )}
+                renderItem={({ item, index }) => (
+                    <PokemonCard pokemon={item}
+                    // totalSelect={totalSelect}
+                    />
+                )}
 
-                    renderItem={({ item, index }) => (
-                        <PokemonCard pokemon={item}
-                        // totalSelect={totalSelect}
-                        />
-                    )}
+                // infinite scroll
+                onEndReached={getList}
+                onEndReachedThreshold={0.4}
 
-                    // infinite scroll
-                    onEndReached={getList}
-                    onEndReachedThreshold={0.4}
-
-                    ListFooterComponent={(
-                        <ActivityIndicator
-                            style={{ height: 100 }}
-                            size={20}
-                            color="grey"
-                        />
-                    )}
-                />
-            </View>
+                ListFooterComponent={(
+                    <ActivityIndicator
+                        style={{ height: 100 }}
+                        size={20}
+                        color="grey"
+                    />
+                )}
+            />
 
         </View>
     )
